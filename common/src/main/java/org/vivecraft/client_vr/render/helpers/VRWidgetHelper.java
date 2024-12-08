41609a4e49538dfdb44185f34d4520a7ced26a6a
fayer3 <bayer.florian@aon.at>
2024-12-08 13:47:07 +0100

    1.21.4 hacky way to get the raw item model

--- common/src/main/java/org/vivecraft/client_vr/render/helpers/VRWidgetHelper.java
+++ common/src/main/java/org/vivecraft/client_vr/render/helpers/VRWidgetHelper.java
@@ -5,18 +5,17 @@ import com.mojang.blaze3d.vertex.*;
 import com.mojang.blaze3d.vertex.VertexFormat.Mode;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.CoreShaders;
-import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.block.model.BakedQuad;
+import net.minecraft.client.renderer.item.ItemStackRenderState;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.client.renderer.texture.TextureAtlas;
-import net.minecraft.client.resources.model.BakedModel;
-import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.RandomSource;
-import net.minecraft.world.inventory.InventoryMenu;
+import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.phys.Vec3;
 import org.joml.Matrix3f;
 import org.joml.Matrix4fStack;
@@ -34,6 +33,8 @@ import java.util.function.Function;
 
 public class VRWidgetHelper {
     private static final RandomSource random = RandomSource.create();
+    private static final ItemStackRenderState ITEM_STATE = new ItemStackRenderState();
+
     public static boolean debug = false;
 
     public static void renderVRThirdPersonCamWidget() {
@@ -94,8 +95,7 @@ public class VRWidgetHelper {
     }
 
     public static void renderVRCameraWidget(float offsetX, float offsetY, float offsetZ, float scale, RenderPass renderPass, ResourceLocation model, ResourceLocation displayModel, Runnable displayBindFunc, Function<Direction, DisplayFace> displayFaceFunc) {
-        // TODO 1.21.4 make models work
-        /*Minecraft minecraft = Minecraft.getInstance();
+        Minecraft minecraft = Minecraft.getInstance();
         ClientDataHolderVR dataholder = ClientDataHolderVR.getInstance();
         Matrix4fStack poseStack = RenderSystem.getModelViewStack();
         poseStack.pushMatrix();
@@ -130,73 +130,85 @@ public class VRWidgetHelper {
         }
         minecraft.gameRenderer.lightTexture().turnOnLightLayer();
 
-        BufferBuilder bufferbuilder = Tesselator.getInstance().begin(Mode.QUADS, DefaultVertexFormat.NEW_ENTITY);
+        BufferBuilder bufferbuilder;
 
         PoseStack poseStack2 = new PoseStack();
         RenderHelper.applyVRModelView(dataholder.currentPass, poseStack2);
         poseStack2.last().pose().identity();
         poseStack2.last().normal().mul(new Matrix3f(dataholder.vrPlayer.vrdata_world_render.getEye(renderPass).getMatrix().toMCMatrix()));
 
-        minecraft.getBlockRenderer().getModelRenderer().renderModel(poseStack2.last(), bufferbuilder, null, minecraft.getModelManager().getModel(model), 1.0F, 1.0F, 1.0F, i, OverlayTexture.NO_OVERLAY);
-        BufferUploader.drawWithShader(bufferbuilder.buildOrThrow());
+        ITEM_STATE.clear();
+        minecraft.getModelManager().getItemModel(model).update(ITEM_STATE, ItemStack.EMPTY, minecraft.getItemModelResolver(), ItemDisplayContext.GROUND, null, null, 0);
+
+        if (!ITEM_STATE.isEmpty() && ITEM_STATE.layers[0].model != null) {
+            bufferbuilder = Tesselator.getInstance().begin(Mode.QUADS, DefaultVertexFormat.NEW_ENTITY);
+            minecraft.getBlockRenderer().getModelRenderer().renderModel(poseStack2.last(), bufferbuilder, null, ITEM_STATE.layers[0].model, 1.0F, 1.0F, 1.0F, i, OverlayTexture.NO_OVERLAY);
+
+            BufferUploader.drawWithShader(bufferbuilder.buildOrThrow());
+        }
 
         RenderSystem.disableBlend();
         displayBindFunc.run();
         RenderSystem.setShader(CoreShaders.RENDERTYPE_ENTITY_SOLID);
 
-        bufferbuilder = Tesselator.getInstance().begin(Mode.QUADS, DefaultVertexFormat.NEW_ENTITY);
+        ITEM_STATE.clear();
+        minecraft.getModelManager().getItemModel(displayModel).update(ITEM_STATE, ItemStack.EMPTY, minecraft.getItemModelResolver(), ItemDisplayContext.GROUND, null, null, 0);
 
-        for (BakedQuad bakedquad : minecraft.getModelManager().getModel(displayModel).getQuads(null, null, random)) {
-            if (displayFaceFunc.apply(bakedquad.getDirection()) != DisplayFace.NONE && bakedquad.getSprite().contents().name().equals(ResourceLocation.parse("vivecraft:transparent"))) {
-                int[] vertexList = bakedquad.getVertices();
-                boolean flag = displayFaceFunc.apply(bakedquad.getDirection()) == DisplayFace.MIRROR;
-                // make normals point up, so they are always bright
-                // TODO: might break with shaders?
-                Vector3f normal = new Matrix3f(poseStack).transform(new Vector3f(0.0F, 1.0F, 0.0F));
-                int j = LightTexture.pack(15, 15);
-                int step = vertexList.length / 4;
-                bufferbuilder.addVertex(
-                        Float.intBitsToFloat(vertexList[0]),
-                        Float.intBitsToFloat(vertexList[1]),
-                        Float.intBitsToFloat(vertexList[2]))
-                    .setColor(1.0F, 1.0F, 1.0F, 1.0F)
-                    .setUv(flag ? 1.0F : 0.0F, 1.0F)
-                    .setOverlay(OverlayTexture.NO_OVERLAY)
-                    .setLight(j)
-                    .setNormal(normal.x, normal.y, normal.z);
-                bufferbuilder.addVertex(
-                        Float.intBitsToFloat(vertexList[step]),
-                        Float.intBitsToFloat(vertexList[step + 1]),
-                        Float.intBitsToFloat(vertexList[step + 2]))
-                    .setColor(1.0F, 1.0F, 1.0F, 1.0F)
-                    .setUv(flag ? 1.0F : 0.0F, 0.0F)
-                    .setOverlay(OverlayTexture.NO_OVERLAY)
-                    .setLight(j)
-                    .setNormal(normal.x, normal.y, normal.z);
-                bufferbuilder.addVertex(
-                        Float.intBitsToFloat(vertexList[step * 2]),
-                        Float.intBitsToFloat(vertexList[step * 2 + 1]),
-                        Float.intBitsToFloat(vertexList[step * 2 + 2]))
-                    .setColor(1.0F, 1.0F, 1.0F, 1.0F)
-                    .setUv(flag ? 0.0F : 1.0F, 0.0F)
-                    .setOverlay(OverlayTexture.NO_OVERLAY)
-                    .setLight(j)
-                    .setNormal(normal.x, normal.y, normal.z);
-                bufferbuilder.addVertex(
-                        Float.intBitsToFloat(vertexList[step * 3]),
-                        Float.intBitsToFloat(vertexList[step * 3 + 1]),
-                        Float.intBitsToFloat(vertexList[step * 3 + 2]))
-                    .setColor(1.0F, 1.0F, 1.0F, 1.0F)
-                    .setUv(flag ? 0.0F : 1.0F, 1.0F)
-                    .setOverlay(OverlayTexture.NO_OVERLAY)
-                    .setLight(j)
-                    .setNormal(normal.x, normal.y, normal.z);
+        if (!ITEM_STATE.isEmpty() && ITEM_STATE.layers[0].model != null) {
+            bufferbuilder = Tesselator.getInstance().begin(Mode.QUADS, DefaultVertexFormat.NEW_ENTITY);
+
+            for (BakedQuad bakedquad : ITEM_STATE.layers[0].model.getQuads(null, null, random)) {
+                if (displayFaceFunc.apply(bakedquad.getDirection()) != DisplayFace.NONE && bakedquad.getSprite().contents().name().equals(ResourceLocation.parse("vivecraft:transparent"))) {
+                    int[] vertexList = bakedquad.getVertices();
+                    boolean flag = displayFaceFunc.apply(bakedquad.getDirection()) == DisplayFace.MIRROR;
+                    // make normals point up, so they are always bright
+                    // TODO: might break with shaders?
+                    Vector3f normal = new Matrix3f(poseStack).transform(new Vector3f(0.0F, 1.0F, 0.0F));
+                    int j = LightTexture.pack(15, 15);
+                    int step = vertexList.length / 4;
+                    bufferbuilder.addVertex(
+                            Float.intBitsToFloat(vertexList[0]),
+                            Float.intBitsToFloat(vertexList[1]),
+                            Float.intBitsToFloat(vertexList[2]))
+                        .setColor(1.0F, 1.0F, 1.0F, 1.0F)
+                        .setUv(flag ? 1.0F : 0.0F, 1.0F)
+                        .setOverlay(OverlayTexture.NO_OVERLAY)
+                        .setLight(j)
+                        .setNormal(normal.x, normal.y, normal.z);
+                    bufferbuilder.addVertex(
+                            Float.intBitsToFloat(vertexList[step]),
+                            Float.intBitsToFloat(vertexList[step + 1]),
+                            Float.intBitsToFloat(vertexList[step + 2]))
+                        .setColor(1.0F, 1.0F, 1.0F, 1.0F)
+                        .setUv(flag ? 1.0F : 0.0F, 0.0F)
+                        .setOverlay(OverlayTexture.NO_OVERLAY)
+                        .setLight(j)
+                        .setNormal(normal.x, normal.y, normal.z);
+                    bufferbuilder.addVertex(
+                            Float.intBitsToFloat(vertexList[step * 2]),
+                            Float.intBitsToFloat(vertexList[step * 2 + 1]),
+                            Float.intBitsToFloat(vertexList[step * 2 + 2]))
+                        .setColor(1.0F, 1.0F, 1.0F, 1.0F)
+                        .setUv(flag ? 0.0F : 1.0F, 0.0F)
+                        .setOverlay(OverlayTexture.NO_OVERLAY)
+                        .setLight(j)
+                        .setNormal(normal.x, normal.y, normal.z);
+                    bufferbuilder.addVertex(
+                            Float.intBitsToFloat(vertexList[step * 3]),
+                            Float.intBitsToFloat(vertexList[step * 3 + 1]),
+                            Float.intBitsToFloat(vertexList[step * 3 + 2]))
+                        .setColor(1.0F, 1.0F, 1.0F, 1.0F)
+                        .setUv(flag ? 0.0F : 1.0F, 1.0F)
+                        .setOverlay(OverlayTexture.NO_OVERLAY)
+                        .setLight(j)
+                        .setNormal(normal.x, normal.y, normal.z);
+                }
             }
+            BufferUploader.drawWithShader(bufferbuilder.buildOrThrow());
         }
-        BufferUploader.drawWithShader(bufferbuilder.buildOrThrow());
         minecraft.gameRenderer.lightTexture().turnOffLightLayer();
         RenderSystem.enableBlend();
-        poseStack.popMatrix();*/
+        poseStack.popMatrix();
     }
 
     public enum DisplayFace {
